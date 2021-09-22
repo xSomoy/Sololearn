fa7a4f63b4465957d557267502155885c7c327b9
John Deadman <xsomoy@tutanota.com>
2021-09-22 06:26:31 +0600

    sql

--- Course Challenges/SQL/Zoo.sql
+++ Course Challenges/SQL/Zoo.sql
@@ -1,5 +1,9 @@
 /* name - "Slim", type - "Giraffe", country_id - 1 */
-
-INSERT INTO Animals (name,type,country_id)
+INSERT INTO Animals (name, type, country_id)
 VALUES ('Slim', 'Giraffe', 1);
-select * from animals INNER JOIN Animals on Animals.country_id
+
+SELECT Animals.name, Animals.type, Countries.country
+FROM Animals
+INNER JOIN Countries
+ON Animals.country_id = Countries.id
+ORDER BY Countries.country;
